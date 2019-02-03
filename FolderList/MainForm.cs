298e8333c92391ce65ab6f2a4d11b82539f64388
lasalesi <lars.mueller@libracore.com>
2019-02-03 17:21:39 +0100

    initial version

---
+++ FolderList/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlgFolder = new FolderBrowserDialog();
+            if (dlgFolder.ShowDialog() == DialogResult.OK)
+            {
+                // read files and add to list
+                dataGridView1.Rows.Clear();
+                // read files
+                DirectoryInfo directory = new DirectoryInfo(dlgFolder.SelectedPath);
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    if (chk_extension.Checked)
+                    {
+                        dataGridView1.Rows.Add(Path.GetFileNameWithoutExtension(file.Name));
+                    }
+                    else
+                    {
+                        dataGridView1.Rows.Add(file.Name);
+                    }
+                }
+            }
+        }
+    }
+}
